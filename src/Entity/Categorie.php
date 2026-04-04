3e9535f264089913446aeb048980b72e74c01483
mayssaferjeni <mayssaferjeni30@gmail.com>
2026-04-04 11:59:51 +0100

    Test de la connectivité avec la base de donnée

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Table(name: "categorie")]
+    #[ORM\Id]
+    #[ORM\Column(name: "id_categorie", type: "integer")]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $idCategorie = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomCategorie = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateCreation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdCategorie(): ?int
+    {
+        return $this->idCategorie;
+    }
+
+    public function setIdCategorie(int $idCategorie): static
+    {
+        $this->idCategorie = $idCategorie;
+
+        return $this;
+    }
+
+    public function getNomCategorie(): ?string
+    {
+        return $this->nomCategorie;
+    }
+
+    public function setNomCategorie(string $nomCategorie): static
+    {
+        $this->nomCategorie = $nomCategorie;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTime
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTime $dateCreation): static
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+}
