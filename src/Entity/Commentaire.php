06a4f0579e1e3771e4f26526ef79888eea740bdb
mayssaferjeni <mayssaferjeni30@gmail.com>
2026-04-04 22:52:16 +0100

    correction erreure d'affichage des post + commentaires

--- src/Entity/Commentaire.php
+++ src/Entity/Commentaire.php
@@ -1,96 +1,37 @@
 <?php
-
 namespace App\Entity;
 
-use App\Repository\CommentaireRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: CommentaireRepository::class)]
+#[ORM\Entity]
+#[ORM\Table(name: "commentaire")]
 class Commentaire
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column]
+    #[ORM\Column(name: "id_commentaire", type: "integer")]
     private ?int $idCommentaire = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $contenu = null;
 
-    #[ORM\Column]
-    private ?\DateTime $dateCommentaire = null;
+    #[ORM\Column(name: "date_commentaire", type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateCommentaire = null;
 
-    #[ORM\Column]
+    #[ORM\Column(name: "id_post", type: "integer")]
     private ?int $idPost = null;
 
-    #[ORM\Column]
+    #[ORM\Column(name: "user_id", type: "integer")]
     private ?int $userId = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getIdCommentaire(): ?int
-    {
-        return $this->idCommentaire;
-    }
-
-    public function setIdCommentaire(int $idCommentaire): static
-    {
-        $this->idCommentaire = $idCommentaire;
-
-        return $this;
-    }
-
-    public function getContenu(): ?string
-    {
-        return $this->contenu;
-    }
-
-    public function setContenu(string $contenu): static
-    {
-        $this->contenu = $contenu;
-
-        return $this;
-    }
-
-    public function getDateCommentaire(): ?\DateTime
-    {
-        return $this->dateCommentaire;
-    }
-
-    public function setDateCommentaire(\DateTime $dateCommentaire): static
-    {
-        $this->dateCommentaire = $dateCommentaire;
-
-        return $this;
-    }
-
-    public function getIdPost(): ?int
-    {
-        return $this->idPost;
-    }
-
-    public function setIdPost(int $idPost): static
-    {
-        $this->idPost = $idPost;
-
-        return $this;
-    }
-
-    public function getUserId(): ?int
-    {
-        return $this->userId;
-    }
-
-    public function setUserId(int $userId): static
-    {
-        $this->userId = $userId;
-
-        return $this;
-    }
+    public function getIdCommentaire(): ?int { return $this->idCommentaire; }
+    public function getContenu(): ?string { return $this->contenu; }
+    public function setContenu(string $contenu): static { $this->contenu = $contenu; return $this; }
+    public function getDateCommentaire(): ?\DateTimeInterface { return $this->dateCommentaire; }
+    public function setDateCommentaire(\DateTimeInterface $d): static { $this->dateCommentaire = $d; return $this; }
+    public function getIdPost(): ?int { return $this->idPost; }
+    public function setIdPost(int $idPost): static { $this->idPost = $idPost; return $this; }
+    public function getUserId(): ?int { return $this->userId; }
+    public function setUserId(int $userId): static { $this->userId = $userId; return $this; }
 }
