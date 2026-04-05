512b10b0c75d6e3de35b4ecc594efb11d9fbcb00
sarrabrahmi <Sarra.Brahmi@esprit.tn>
2026-04-05 20:25:09 +0100

    gestion meditation itegration

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LibraryController extends AbstractController
+{
+    #[Route('/library', name: 'library')]
+    public function index(): Response
+    {
+        return $this->render('library/index.html.twig');
+    }
+}
